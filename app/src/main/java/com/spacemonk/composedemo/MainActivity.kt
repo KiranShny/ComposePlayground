8c3f91dbf9b0613c71ac391b24ad10f7215e13cc
KiranShny <ikiran1997>
2021-10-25 23:05:45 +0530

    Adds Offers carousel

--- app/src/main/java/com/spacemonk/composedemo/MainActivity.kt
+++ app/src/main/java/com/spacemonk/composedemo/MainActivity.kt
@@ -16,97 +16,122 @@ import androidx.compose.material.icons.rounded.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Outline
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Density
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
+import com.airbnb.lottie.model.content.GradientColor
+import com.google.accompanist.pager.ExperimentalPagerApi
 import com.spacemonk.andromeda.compose.AndromedaComposeTheme
+import com.spacemonk.andromeda.compose.navbar.AndromedaNavBar
+import com.spacemonk.composedemo.exts.gradientBackground
 import com.spacemonk.composedemo.model.Category
+import com.spacemonk.composedemo.model.OffersModel
 import com.spacemonk.composedemo.model.RecentOrder
-import com.spacemonk.composedemo.ui.theme.ComposeDemoTheme
 import com.spacemonk.composedemo.ui.theme.parse
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.selects.whileSelect
+
+private val staticCategories = listOf(
+    Category(
+        title = "Generic",
+        imageUrl = "https://pharmacyone-images.s3.ap-south-1.amazonaws.com/app-images/capsule.png",
+        backgroundColor = "#fff6f2"
+    ),
+    Category(
+        title = "Ayurvedic",
+        imageUrl = "https://pharmacyone-images.s3.ap-south-1.amazonaws.com/app-images/ayurveda.png",
+        backgroundColor = "#e9fff5"
+    ),
+    Category(
+        title = "Vaccines",
+        imageUrl = "https://pharmacyone-images.s3.ap-south-1.amazonaws.com/app-images/medicine.png",
+        backgroundColor = "#f1fcff"
+    ),
+    Category(
+        title = "Surgical",
+        imageUrl = "https://pharmacyone-images.s3.ap-south-1.amazonaws.com/app-images/surgical-instrument.png",
+        backgroundColor = "#f1f5ff"
+    ),
+    Category(
+        title = "Cosmetics",
+        imageUrl = "https://pharmacyone-images.s3.ap-south-1.amazonaws.com/app-images/skincare.png",
+        backgroundColor = "#fffde9"
+    ),
+    Category(
+        title = "SADSSDAJ",
+        imageUrl = "https://pharmacyone-images.s3.ap-south-1.amazonaws.com/app-images/skincare.png",
+        backgroundColor = "#fffdee"
+    )
+)
+private val recentOrderData = listOf(
+    RecentOrder(
+        title = "Crocin 650",
+        distributor = "Mercury Agencies",
+        amountAfter = "$30",
+        amountBefore = "$70",
+        availability = "130 Available",
+        imageUrl = "https://static2.medplusmart.com/products/_a17f3e_/DOLO0011_L.jpg"
+    ),
+    RecentOrder(
+        title = "Festal N Strip Of 10 Tablet",
+        distributor = "Mercury Agencies",
+        amountAfter = "$30",
+        amountBefore = "$70",
+        availability = "130 Available",
+        imageUrl = "https://static2.medplusmart.com/products/_25d684_/PRIM0007_T.jpg"
+    )
+)
+private val availableStaticOffers = listOf(
+    OffersModel(
+        title = "SUN Pharma",
+        subtitle = "Get 10% off for all the SUN Pharma purchase"
+    ),
+    OffersModel(
+        title = "SUN Pharma 2",
+        subtitle = "Get 20% off for all the SUN Pharma purchase"
+    ),
+    OffersModel(
+        title = "SUN Pharma 3",
+        subtitle = "Get 25% off for all the SUN Pharma purchase"
+    )
+)
 
 class MainActivity : ComponentActivity() {
-    private val staticCategories = listOf(
-        Category(
-            title = "Generic",
-            imageUrl = "https://pharmacyone-images.s3.ap-south-1.amazonaws.com/app-images/capsule.png",
-            backgroundColor = "#fff6f2"
-        ),
-        Category(
-            title = "Ayurvedic",
-            imageUrl = "https://pharmacyone-images.s3.ap-south-1.amazonaws.com/app-images/ayurveda.png",
-            backgroundColor = "#e9fff5"
-        ),
-        Category(
-            title = "Vaccines",
-            imageUrl = "https://pharmacyone-images.s3.ap-south-1.amazonaws.com/app-images/medicine.png",
-            backgroundColor = "#f1fcff"
-        ),
-        Category(
-            title = "Surgical",
-            imageUrl = "https://pharmacyone-images.s3.ap-south-1.amazonaws.com/app-images/surgical-instrument.png",
-            backgroundColor = "#f1f5ff"
-        ),
-        Category(
-            title = "Cosmetics",
-            imageUrl = "https://pharmacyone-images.s3.ap-south-1.amazonaws.com/app-images/skincare.png",
-            backgroundColor = "#fffde9"
-        ),
-        Category(
-            title = "SADSSDAJ",
-            imageUrl = "https://pharmacyone-images.s3.ap-south-1.amazonaws.com/app-images/skincare.png",
-            backgroundColor = "#fffdee"
-        )
-    )
-    private val recentOrderData = listOf(
-        RecentOrder(
-            title = "Crocin 650",
-            distributor = "Mercury Agencies",
-            amountAfter = "$30",
-            amountBefore = "$70",
-            availability = "130 Available",
-            imageUrl = "https://static2.medplusmart.com/products/_a17f3e_/DOLO0011_L.jpg"
-        ),
-        RecentOrder(
-            title = "Festal N Strip Of 10 Tablet",
-            distributor = "Mercury Agencies",
-            amountAfter = "$30",
-            amountBefore = "$70",
-            availability = "130 Available",
-            imageUrl = "https://static2.medplusmart.com/products/_25d684_/PRIM0007_T.jpg"
-        )
-    )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AndromedaComposeTheme {
-                MainScreen(
-                    categories = staticCategories,
-                    recentOrders = recentOrderData
-                )
+                MainScreen()
             }
         }
     }
 }
 
+@Preview(name = "Full Screen")
 @Composable
-fun MainScreen(categories: List<Category>, recentOrders: List<RecentOrder>) {
+fun MainScreenDemo() {
+    AndromedaComposeTheme {
+        MainScreen()
+    }
+}
+
+
+@Composable
+fun MainScreen(
+    categories: List<Category> = staticCategories,
+    recentOrders: List<RecentOrder> = recentOrderData,
+    availableOffers: List<OffersModel> = availableStaticOffers
+) {
     Scaffold(
         topBar = {
-            TopAppBar(
-                title = { Text("Andromeda") },
-                backgroundColor = MaterialTheme.colors.background
+            AndromedaNavBar(
+                title = "Andromeda",
+                backgroundColor = AndromedaComposeTheme.colors.fillActiveSecondary
             )
         }
     ) {
@@ -117,6 +142,7 @@ fun MainScreen(categories: List<Category>, recentOrders: List<RecentOrder>) {
                 recentOrders,
                 modifier = Modifier.padding(0.dp, 16.dp)
             )
+            Offers(offers = availableOffers)
         }
     }
 }
@@ -294,3 +320,68 @@ fun CategoryCard(categoryData: Category) {
         )
     }
 }
+
+@ExperimentalCoroutinesApi
+@Composable
+fun Offers(offers: List<OffersModel>) {
+    LazyRow {
+        itemsIndexed(
+            items = offers,
+            itemContent = { _, item ->
+                OffersCard(item)
+            }
+        )
+        /*whileSelect {
+
+        }*/
+    }
+    LazyRow(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        itemsIndexed(
+            items = offers,
+            itemContent = { _, _ ->
+                Text(text = "•")
+            }
+        )
+    }
+}
+
+@Composable
+fun OffersCard(item: OffersModel) {
+    Card(
+        modifier = Modifier
+            .width(330.dp)
+            .padding(8.dp),
+        shape = RoundedCornerShape(10.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .gradientBackground(
+                    colors = listOf(
+                        Color.parse("#00b08c"),
+                        Color.parse("#35e2bf")
+                    ),
+                    angle = 270f
+                )
+        ) {
+            Text(
+                text = item.title,
+                fontSize = 12.sp,
+                color = AndromedaComposeTheme.colors.staticWhiteFontColor,
+                modifier = Modifier.padding(top = 16.dp, start = 16.dp, bottom = 8.dp, end = 16.dp)
+            )
+            Text(
+                text = item.subtitle,
+                fontSize = 22.sp,
+                modifier = Modifier.padding(top = 8.dp, start = 16.dp, bottom = 16.dp, end = 16.dp),
+                color = AndromedaComposeTheme.colors.staticWhiteFontColor,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
+
+@Composable
---
+++ app/src/main/java/com/spacemonk/composedemo/model/OffersModel.kt
@@ -0,0 +1,6 @@
+package com.spacemonk.composedemo.model
+
+data class OffersModel(
+    val title: String,
+    val subtitle: String
+)
